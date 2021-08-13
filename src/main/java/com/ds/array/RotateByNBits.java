a743235ad7e27ff42f1b8b7c58ef9f7a0cfce053
anupam-kundu <anupamkundu3@gmail.com>
2021-08-13 10:31:31 +0530

    added new prog files

---
+++ src/main/java/com/ds/array/RotateByNBits.java
@@ -0,0 +1,17 @@
+package com.ds.array;
+
+/**
+ * 
+ * rotate an array by N bits 
+ * 1. Clockwise
+ * 2. Anti clockwise
+ * 
+ */
+
+public class RotateByNBits {
+
+    public static void main(String[] args) {
+        System.out.println("Rorate Array By N bits");
+             
+    }
+}
---
+++ src/main/java/com/ds/linkedList/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+package com.ds.linkedList;
+
+/**
+ * Remove Duplicate elements from a linked list
+ * 
+ */
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        
+    }
+}
