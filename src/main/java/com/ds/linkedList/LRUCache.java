2f925b340371ba1f307b48465f3ab154559efede
anupam-veedna <anupam@veedna.com>
2023-04-12 21:55:31 +0530

    added lru cache default template

---
+++ src/main/java/com/ds/linkedList/LRUCache.java
@@ -0,0 +1,16 @@
+package com.ds.linkedList;
+
+/**
+ * Implement LRU cache of constant size n
+ *
+ * using combination of HashMap and doubly link list
+ *
+ * time complexity O(1)
+ * space complexity O(n)
+ *
+ * map will tell if element is present or not , link-list will hold elements in order of access
+ * remove element from front of the list
+ * add or move element to the rear of the list if it is accessed
+ */
+public class LRUCache {
+}
