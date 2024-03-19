fff108874148c96b0efc678c33e4a404ec91db5f
anupam-veedna <anupam@veedna.com>
2024-03-19 21:15:24 +0530

    added lru cache code

--- src/main/java/com/ds/linkedList/LRUCache.java
+++ src/main/java/com/ds/linkedList/LRUCache.java
@@ -1,5 +1,10 @@
 package com.ds.linkedList;
 
+import lombok.EqualsAndHashCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Implement LRU cache of constant size n
  *
@@ -13,4 +18,58 @@ package com.ds.linkedList;
  * add or move element to the rear of the list if it is accessed
  */
 public class LRUCache {
+    private final int SIZE = 5;
+    @EqualsAndHashCode
+    private class DLLNode {
+        int data;
+
+        @EqualsAndHashCode.Exclude
+        DLLNode prev;
+        @EqualsAndHashCode.Exclude
+        DLLNode next;
+    }
+
+    private Map<Integer, DLLNode> lruMap = new HashMap<>();
+    private DLLNode head, tail;
+
+    private int getData(int key) {
+        if(lruMap.containsKey(key)){
+            updateAccess(key);
+        } else {
+            addData(key);
+        }
+        return key;
+    }
+
+    private void addData(int key) {
+        if(lruMap.size() >= SIZE) {
+            lruMap.remove(head.data);
+            head.next.prev = null;
+            head = head.next;
+        }
+        DLLNode temp = new DLLNode();
+        temp.data = key;
+        temp.prev = tail;
+        tail = temp;
+        lruMap.put(key, temp);
+    }
+
+    private void updateAccess(int key) {
+        DLLNode temp = lruMap.get(key);
+        if(temp.prev == null) {
+            head = temp.next;
+            head.prev = null;
+            tail.next = temp;
+            temp.prev = tail;
+            temp.next = null;
+            tail = temp;
+        } else {
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            temp.prev = tail;
+            tail.next = temp;
+            temp.next = null;
+            tail = temp;
+        }
+    }
 }
