14248b1341e228ff3d8f5573e2f94f3069e636ec
anupam-kundu <anupamkundu3@gmail.com>
2025-05-16 21:37:33 +0530

    added array rotation

--- src/main/java/com/ds/array/RotateByNBits.java
+++ src/main/java/com/ds/array/RotateByNBits.java
@@ -1,5 +1,10 @@
 package com.ds.array;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * 
  * rotate an array by N bits 
@@ -12,6 +17,30 @@ public class RotateByNBits {
 
     public static void main(String[] args) {
         System.out.println("Rorate Array By N bits");
-             
+        int[] input1 = new int[] {1,2,3,4,5};
+        int[] input2 = Arrays.copyOf(input1, input1.length);
+        RotateByNBits obj = new RotateByNBits();
+        obj.clockwise(input1, 3);
+        obj.antiClockwise(input2, 3);
+    }
+
+    private void clockwise(int[] a, int n) {
+        int offset = n % a.length;
+        int[] prefixArray = Arrays.copyOfRange(a, a.length - offset, a.length);
+//        System.out.println(Arrays.stream(prefixArray).boxed().collect(Collectors.toList()));
+        int[] suffixArray = Arrays.copyOfRange(a, 0, a.length - offset);
+//        System.out.println(Arrays.stream(suffixArray).boxed().collect(Collectors.toList()));
+        int[] result = ArrayUtils.addAll(prefixArray, suffixArray);
+        System.out.println(Arrays.stream(result).boxed().collect(Collectors.toList()));
+    }
+
+    private void antiClockwise(int[] a, int n) {
+        int offset = n % a.length;
+        int[] prefixArray = Arrays.copyOfRange(a, offset, a.length);
+//        System.out.println(Arrays.stream(prefixArray).boxed().collect(Collectors.toList()));
+        int[] suffixArray = Arrays.copyOfRange(a, 0, offset);
+//        System.out.println(Arrays.stream(suffixArray).boxed().collect(Collectors.toList()));
+        int[] result = ArrayUtils.addAll(prefixArray, suffixArray);
+        System.out.println(Arrays.stream(result).boxed().collect(Collectors.toList()));
     }
 }
