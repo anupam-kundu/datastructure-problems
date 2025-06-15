099dda5a8e48bdca9c987b9adf0efabf91f86f32
anupam-kundu <anupamkundu3@gmail.com>
2025-06-15 12:17:26 +0530

    added word 2 number

--- src/main/java/com/ds/general/Word2Number.java
+++ src/main/java/com/ds/general/Word2Number.java
@@ -1,9 +1,6 @@
 package com.ds.general;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Ten Thousand => 10,000
@@ -32,60 +29,82 @@ public class Word2Number {
         digits.put("seventeen", 17);
         digits.put("eighteen", 18);
         digits.put("nineteen", 19);
+        digits.put("twenty", 20);
+        digits.put("thirty", 30);
+        digits.put("forty", 40);
+        digits.put("fifty", 50);
+        digits.put("sixty", 60);
+        digits.put("seventy", 70);
+        digits.put("eighty", 80);
+        digits.put("ninety", 90);
     }
 
-    private Map<String, Integer> multiplier = new HashMap<>();
+    private Map<String, Long> multiplier = new HashMap<>();
     {
-        multiplier.put("ten", 10);
-        multiplier.put("twenty", 20);
-        multiplier.put("thirty", 30);
-        multiplier.put("forty", 40);
-        multiplier.put("fifty", 50);
-        multiplier.put("sixty", 60);
-        multiplier.put("seventy", 70);
-        multiplier.put("eighty", 80);
-        multiplier.put("ninety", 90);
-        multiplier.put("hundred", 100);
-        multiplier.put("thousand", 1000);
-        multiplier.put("lac", 100000);
-        multiplier.put("million", 1000000);
-        multiplier.put("billion", 1000000000);
+        multiplier.put("hundred", 100L);
+        multiplier.put("thousand", 1000L);
+        multiplier.put("lac", 100000L);
+        multiplier.put("million", 1000000L);
+        multiplier.put("billion", 1000000000L);
+        multiplier.put("trillion", 1000000000000L);
     }
-    public void word2number(String word) {
-        int currentNumber = 0;
-        List<Integer> calculatedNumbers = new ArrayList<>();
-        boolean isDigit = false;
-        String [] splits = word.split(" ");
-        for(int i = 0; i< splits.length;i++ ){
-            String currentWord = splits[i].toLowerCase();
-            if(digits.containsKey(currentWord)) {
-                currentNumber += digits.get(currentWord);
-                isDigit = true;
-            } else if (multiplier.containsKey(currentWord)) {
-                currentNumber = currentNumber == 0 ? multiplier.get(currentWord) : currentNumber *  multiplier.get(currentWord);
-                if(isDigit) { // multiplier after digit , so move to calculated number
-                    calculatedNumbers.add(currentNumber);
-                    currentNumber = 0;
+    public void word2number(String input) {
+        boolean isValidInput = true;
+        long result = 0;
+        long finalResult = 0;
+        List<String> allowedStrings = Arrays.asList
+                (
+                        "zero","one","two","three","four","five","six","seven",
+                        "eight","nine","ten","eleven","twelve","thirteen","fourteen",
+                        "fifteen","sixteen","seventeen","eighteen","nineteen","twenty",
+                        "thirty","forty","fifty","sixty","seventy","eighty","ninety",
+                        "hundred","thousand","million","billion","trillion"
+                );
+
+//        String input="One hundred two thousand and thirty four";
+
+        if(input != null && input.length()> 0)
+        {
+            input = input.replaceAll("-", " ");
+            input = input.toLowerCase().replaceAll(" and", " ");
+            String[] splittedParts = input.trim().split("\\s+");
+
+            for(String str : splittedParts)
+            {
+                if(!allowedStrings.contains(str))
+                {
+                    isValidInput = false;
+                    System.out.println("Invalid word found : "+str);
+                    break;
                 }
-                isDigit = false;
-            } else {
-                throw new RuntimeException("perser not found for : "+currentWord);
             }
-            System.out.println("Current Iteration, word: " + currentWord + " number: " + currentNumber + " calculated: " + calculatedNumbers);
+            if(isValidInput)
+            {
+                for(String str : splittedParts)
+                {
+                    if(digits.containsKey(str)) {
+                        result += digits.get(str);
+                    }
+                    else if(str.equalsIgnoreCase("hundred")) {
+                        result *= 100;  // because hundred can come in million / billion s
+                    }
+                    else if(multiplier.containsKey(str)) {
+                        result *= multiplier.get(str);
+                        finalResult += result;
+                        result=0;
+                    }
+                }
+
+                finalResult += result;
+                result=0;
+                System.out.println(finalResult);
+            }
         }
-        if(currentNumber > 0) {
-            calculatedNumbers.add(currentNumber);
-            currentNumber = 0;
-        }
-        System.out.println(calculatedNumbers);
-        int result = 0;
-        for(int i =0; i< calculatedNumbers.size() ; i++) {
-            result += calculatedNumbers.get(i);
-        }
-        System.out.println("Result : " + result);
     }
     public static void main(String[] args) {
         Word2Number obj = new Word2Number();
+        obj.word2number("three thousand two hundred forty seven");
         obj.word2number("fifteen million twenty five thousand three hundred forty seven");
+        obj.word2number("Two Billion One Hundred Forty Seven Million Four Hundred Eighty Three Thousand Six Hundred Forty Seven");
     }
 }
